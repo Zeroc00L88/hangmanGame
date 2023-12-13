a3cab5ba5002759ca79c81e8a8d7ab86db75e365
Jerome <jeromelesne@gmail.com>
2023-12-13 13:31:25 +0100

    project working

--- assets/script/script.js
+++ assets/script/script.js
@@ -23,27 +23,27 @@ let words = [
 
 words = words.map((e) => e.toUpperCase());
 let currentWord = [];
+let falseCount = 0;
+let goodCount = 0;
 
 const play = document.querySelector("#playButton");
 const startWin = document.querySelector("#startWin");
 const secretWord = document.querySelector("#secretWord");
 const keys = document.querySelectorAll(".key");
 const lostRestartButton = document.querySelector("#lostRestartButton");
-const lostWin = document.querySelector("#lostWin");
+const restartWin = document.querySelector("#restartWin");
+const restartWinMsg = document.querySelector("#restartWin h2");
 const imgDiv = document.querySelectorAll("#hangman div img");
+const vkbd = document.querySelector("#vkbd p");
+// console.log(vkbd);
+// console.log(imgDiv);
+// const keyPlaceHolder = document.createElement("p");
 
 function removeAllChildNodes(parent) {
     while (parent.firstChild) {
         parent.removeChild(parent.firstChild);
     }
 }
-
-const startGame = () => {
-    play.addEventListener("click", () => {
-        startWin.classList.toggle("hidden");
-        game();
-    });
-};
 
 const getRandom = (min, max) => {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -66,16 +66,23 @@ const displayWordPlaceholder = (word, placeholderChar) => {
     });
 };
 
-const checkEachkey = (word, elmt, falseCount) => {
-    elmt.style.color = "rgba(0, 0, 0, 0)";
-    idx = word.indexOf(elmt.innerHTML);
+const checkEachkey = (elmt) => {
+    elmt.style.color = "rgba(0,0,0,0)";
+    elmt.style.pointerEvents = "none";
+    idx = currentWord.indexOf(elmt.innerHTML);
     if (idx != -1) {
         while (idx != -1) {
+            goodCount++;
+            console.log("goodcount :", goodCount);
             console.log(elmt.innerHTML);
             console.log(idx);
             let charChange = document.querySelectorAll("#secretWord p");
             charChange[idx].innerHTML = elmt.innerHTML;
-            idx = word.indexOf(elmt.innerHTML, idx + 1);
+            idx = currentWord.indexOf(elmt.innerHTML, idx + 1);
+        }
+        if (goodCount == currentWord.length) {
+            goodCount = 0;
+            restart("Vous avez gagné");
         }
     } else {
         if (falseCount < 8) {
@@ -84,43 +91,56 @@ const checkEachkey = (word, elmt, falseCount) => {
         } else {
             displayHangman(falseCount);
             falseCount = 0;
-            lostRestart();
+            restart("Vous avez perdu");
         }
     }
-    return falseCount;
 };
 
-const checkKey = (word) => {
-    let falseCount = 0;
-    console.log(falseCount);
+const checkKey = () => {
     keys.forEach((elmt) => {
         elmt.style.color = "lightgrey";
         elmt.addEventListener("click", function eventkey() {
-            falseCount = checkEachkey(word, elmt, falseCount);
+            checkEachkey(elmt);
         });
     });
 };
 
-const lostRestart = () => {
+const restart = (msg) => {
+    restartWinMsg.innerHTML = msg;
     imgDiv.forEach((e) => e.classList.add("hidden"));
-    lostWin.classList.toggle("hidden");
+    keys.forEach((e) => {
+        e.style.pointerEvents = "none";
+        e.style.color = "lightgrey";
+    });
+    restartWin.classList.toggle("hidden");
     currentWord = [];
     let secretWordAll = document.getElementById("secretWord");
     removeAllChildNodes(secretWordAll);
     lostRestartButton.addEventListener(
         "click",
         () => {
-            lostWin.classList.add("hidden");
+            keys.forEach((e) => {
+                e.style.pointerEvents = "auto";
+            });
+            restartWin.classList.add("hidden");
             game();
         },
         { once: true },
     );
 };
+
+const startGame = () => {
+    play.addEventListener("click", () => {
+        startWin.classList.toggle("hidden");
+        game();
+        checkKey();
+    });
+};
+
 const game = () => {
     currentWord = getRandomWord(words);
-    displayWordPlaceholder(currentWord, "_");
     console.log(currentWord);
-    checkKey(currentWord);
+    displayWordPlaceholder(currentWord, "_");
 };
 
 startGame();
